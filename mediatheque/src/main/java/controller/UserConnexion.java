ac5392f08f3c35dffa45c4bec05533f928a50c74
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-08-23 16:49:12 +0200

    mid

--- mediatheque/src/main/java/controller/UserConnexion.java
+++ mediatheque/src/main/java/controller/UserConnexion.java
@@ -27,13 +27,24 @@ public class UserConnexion implements Serializable {
     @Inject
     private DaoUtillisateur daoUtillisateur;
     private Utilisateur currentUser;
-    private String user;
+    private String inputPseudo;
     private String inputPassword;
+    private String inputFirstName;
+    private String inputSecondName;
+    private String inputBirthDate;
+    private String inputPostalCode;
+    private String inputEmail;
+    private String inputStreetNumber;
+    private String inputStreetName;
     private String EncryptedPassword;
     
-    
-    public boolean comparePassword() {
-        return EncryptedPassword.equals(getUserPassword());
+    public String connexion() throws NoSuchAlgorithmException {
+        encryptPassword();
+        if (comparePassword()) {
+            currentUser = findUserByPseudo();
+            return "protected/userPages.xhtml";
+        }
+        return "connexionError";
     }
     
     private void encryptPassword() throws NoSuchAlgorithmException {
@@ -44,28 +55,100 @@ public class UserConnexion implements Serializable {
         EncryptedPassword = tmpPassword;
     }
     
+    private boolean comparePassword() {
+        return EncryptedPassword.equals(getUserPassword());
+    }
+    
     private String getUserPassword() {
-        return daoUtillisateur.findByPseudo(user).getMotPasse();
+        return daoUtillisateur.findByPseudo(inputPseudo).getMotPasse();
+    }
+    
+    private Utilisateur findUserByPseudo() {
+        return daoUtillisateur.findByPseudo(inputPseudo);
+    }
+    
+    public void insertUser() {
+        
+    }
+    
+    public Utilisateur getCurrentUser() {
+        return currentUser;
     }
 
-    public String getUser() {
-        return user;
+    public void setCurrentUser(Utilisateur currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public String getInputPseudo() {
+        return inputPseudo;
+    }
+
+    public void setInputPseudo(String inputPseudo) {
+        this.inputPseudo = inputPseudo;
     }
 
     public String getInputPassword() {
         return inputPassword;
     }
 
-    public void setInputPassword(String password) {
-        this.inputPassword = password;
+    public void setInputPassword(String inputPassword) {
+        this.inputPassword = inputPassword;
     }
 
-    public String getEncryptedPassword() {
-        return EncryptedPassword;
+    public String getInputFirstName() {
+        return inputFirstName;
     }
 
-    public void setEncryptedPassword(String EncryptedPassword) {
-        this.EncryptedPassword = EncryptedPassword;
+    public void setInputFirstName(String inputFirstName) {
+        this.inputFirstName = inputFirstName;
     }
-    
+
+    public String getInputSecondName() {
+        return inputSecondName;
+    }
+
+    public void setInputSecondName(String inputSecondName) {
+        this.inputSecondName = inputSecondName;
+    }
+
+    public String getInputBirthDate() {
+        return inputBirthDate;
+    }
+
+    public void setInputBirthDate(String inputBirthDate) {
+        this.inputBirthDate = inputBirthDate;
+    }
+
+    public String getInputPostalCode() {
+        return inputPostalCode;
+    }
+
+    public void setInputPostalCode(String inputPostalCode) {
+        this.inputPostalCode = inputPostalCode;
+    }
+
+    public String getInputEmail() {
+        return inputEmail;
+    }
+
+    public void setInputEmail(String inputEmail) {
+        this.inputEmail = inputEmail;
+    }
+
+    public String getInputStreetNumber() {
+        return inputStreetNumber;
+    }
+
+    public void setInputStreetNumber(String inputStreetNumber) {
+        this.inputStreetNumber = inputStreetNumber;
+    }
+
+    public String getInputStreetName() {
+        return inputStreetName;
+    }
+
+    public void setInputStreetName(String inputStreetName) {
+        this.inputStreetName = inputStreetName;
+    }
+
 }
