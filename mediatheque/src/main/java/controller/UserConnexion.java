24edaa70cb8f1774151f1a653aec6cef71b0946e
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-08-23 09:18:04 +0200

    mid

--- mediatheque/src/main/java/controller/UserConnexion.java
+++ mediatheque/src/main/java/controller/UserConnexion.java
@@ -5,10 +5,41 @@
  */
 package controller;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 /**
  *
  * @author Adrien
  */
 public class UserConnexion {
+
+    private String user;
+    private String password;
+    
+    private void encryptPassword() throws NoSuchAlgorithmException {
+        String tmpPassword = getPassword();
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] encodeHash = messageDigest.digest(tmpPassword.getBytes(StandardCharsets.UTF_8));
+        tmpPassword = Base64.getEncoder().encodeToString(encodeHash);
+        setPassword(tmpPassword);
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
     
 }
